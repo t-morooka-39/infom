09a08f2b6b98497f588a374c0fe82670b6e070e2
morookaT <morooka.olopatadine@gmail.com>
2019-07-26 09:01:13 +0900

    [add][update]deviseのviewsとmigrateを追加し、リンクを作成

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,6 @@ class Member < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         :confirmable, :lockable, :timeoutable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable
 end
--- db/migrate/20190725132119_devise_create_members.rb
+++ db/migrate/20190725132119_devise_create_members.rb
@@ -15,22 +15,22 @@ class DeviseCreateMembers < ActiveRecord::Migration[5.2]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
 
 
       t.timestamps null: false
@@ -38,7 +38,7 @@ class DeviseCreateMembers < ActiveRecord::Migration[5.2]
 
     add_index :members, :email,                unique: true
     add_index :members, :reset_password_token, unique: true
-    # add_index :members, :confirmation_token,   unique: true
-    # add_index :members, :unlock_token,         unique: true
+    add_index :members, :confirmation_token,   unique: true
+    add_index :members, :unlock_token,         unique: true
   end
 end
