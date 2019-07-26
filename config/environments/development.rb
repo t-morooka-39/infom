cba414a059bc8f1fa76f3875a5932fae7b5d6e0b
morookaT <morooka.olopatadine@gmail.com>
2019-07-26 15:58:03 +0900

    [update]gmailで認証するように変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "morooka.olopatadine@gmail.com",
+    :password => "<%= ENV['GMAIL_PASSWORD'] %>",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
