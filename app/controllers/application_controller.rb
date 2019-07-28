333b10ec2accdb28c42003c37f7be666f017e87e
morookaT <morooka.olopatadine@gmail.com>
2019-07-28 16:34:29 +0900

    [fix][add]バリデーションを修正、シードを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
@@ -8,5 +10,6 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:account_update, keys: [:first_name])
       devise_parameter_sanitizer.permit(:sign_up, keys: [:sex])
       devise_parameter_sanitizer.permit(:account_update, keys: [:sex])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:admin])
     end
 end
