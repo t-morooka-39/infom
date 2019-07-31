439408c136305109782d554d1634b53928dbe954
morookaT <morooka.olopatadine@gmail.com>
2019-07-31 15:40:28 +0900

    [update]sccsを修正、slimを修正
    tweetのshowを修正、マイページのeditをパスワードを打ち込まずに修正できるようにした

--- Gemfile
+++ Gemfile
@@ -80,3 +80,4 @@ gem 'devise-i18n'
 gem 'devise-i18n-views'
 gem "kaminari"
 gem "kaminari-i18n"
+gem "mini_magick", '>= 4.8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
   
-  devise_for :members
+  devise_for :members,controllers: {
+    registrations: 'members/registrations',
+    #他にもカスタマイズするコントローラーがあればここに記述する
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "top#index"
   resources :members
