4b8f467c927a2e674ff08adfb91b97adee78d6ca
morookaT <morooka.olopatadine@gmail.com>
2019-09-01 21:33:00 +0900

    [fix]systemspecが動かない問題の解消
    js:trueが不足し動いていなかった。docker-composeも改良

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -1,15 +1,27 @@
 require 'rails_helper'
 
-RSpec.feature 'Tweets', type: :system do
-  scenario ' ユーザーが新しいツイートを作成する ' do
-    member = FactoryBot.create(:member)
+RSpec.feature 'Tweets', type: :system, js: true do
+  let!(:member) { FactoryBot.create(:member) }
+  #   find(".fa-send-o").click
+  #   expect{
+  #     fill_in "tweet_body", with: "本文テスト"
+  #     click_button "投稿する"
+
+  #     expect(page).to have_content '投稿しました。'
+  #     expect(page).to have_content '本文テスト'
+  #     expect(page).to have_content "#{member.name}"
+
+  #   }.to change(member.tweets, :count).by(1)
+  # end
+  before {
     visit root_path
-    wait_until { page.has_css?("h1", text: "infomus") }
+    wait_until { page.has_css?("h1", text: "Infomus") }
     click_link 'ログインはこちら'
     fill_in 'member_email', with: member.email
     fill_in 'member_password', with: member.password
     click_button "ログイン"
-
+  }
+  scenario ' ユーザーが新しいツイートを作成する ' do
     find(".fa-send-o").click
     expect{
       fill_in "tweet_body", with: "本文テスト"
@@ -18,7 +30,15 @@ RSpec.feature 'Tweets', type: :system do
       expect(page).to have_content '投稿しました。'
       expect(page).to have_content '本文テスト'
       expect(page).to have_content "#{member.name}"
-
     }.to change(member.tweets, :count).by(1)
   end
+  # context ' ユーザーが新しいツイートを作成する ' do
+  #   it {
+  #     fill_in "tweet_body", with: "本文テスト"
+  #     click_button "投稿する"
+  #     expect(page).to have_content '投稿しました。'
+  #     expect(page).to have_content '本文テスト'
+  #     expect(page).to have_content "#{member.name}"
+  #   }
+  #   end
 end 
