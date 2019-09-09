4d7f022b753fd28d334fb1b7fe95631883b27021
morookaT <morooka.olopatadine@gmail.com>
2019-09-09 14:07:52 +0900

    [update]コメント機能のテストを追記

--- spec/system/post_tweet_spec.rb
+++ spec/system/post_tweet_spec.rb
@@ -6,30 +6,27 @@ describe 'Tweets', type: :system, js: true do
     before do 
       token = member.confirmation_token
       visit member_confirmation_path(confirmation_token: token)
-      page.driver.browser.manage.window.resize_to(800,550)
       # expect(page).to have_content 'メールが確認され、登録が完了しました。'
       fill_in 'member_email', with: "#{member.email}"
       fill_in 'member_password', with: "#{member.password}"
       click_button 'ログイン'
-      expect(page).to have_content 'ログインしました。'
+      visit new_tweet_path
     end
     it 'ツイートが投稿できること' do
-      visit new_tweet_path
-      expect(page).to have_content 'ツイート作成'
       fill_in "tweet_body", with: "本文テスト"
-      # click_button "投稿する"
-      # expect(page).to have_content '投稿しました。'
+      click_button "投稿する"
+      expect(page).to have_content '投稿しました。'
+      expect(page).to have_content '本文テスト'
+      expect(page).to have_content "#{member.first_name}"
     end
-    # expect(page).to have_content '本文テスト'
-    # expect(page).to have_content "#{member.first_name}"
+    it '投稿したツイートにコメントできること' do
+      fill_in "tweet_body", with: "本文テスト"
+      click_button "投稿する"
+      fill_in 'comment_body', with: 'コメントテスト'
+      click_button '送信'
+      expect(page).to have_content 'コメントを投稿しました'
+      expect(page).to have_content 'コメントテスト'
+      expect(page).to have_selector 'span', text: "#{member.first_name}"
+    end
   end
-
-  # it '投稿したツイートにコメントが打てること' do
-
-  #   fill_in 'comment_body', with: 'コメントテスト'
-  #   click_button '送信'
-  #   expect(page).to have_content 'コメントを投稿しました'
-  #   expect(page).to have_content 'コメントテスト'
-  #   expect(page).to have_selector 'span', text: "#{member.first_name}"
-  # end
 end 
