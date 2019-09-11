2b1872c2595e8c8f33239d8fee79530e94f3e975
morookaT <morooka.olopatadine@gmail.com>
2019-09-11 11:28:26 +0900

    [add]画像プレビューを表示する機能の追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,21 @@
 //= require turbolinks
 //= require_tree .
 
+$("#file").on("change",'input[type="file"]', function(e) {
+  var file = e.target.files[0],
+      reader = new FileReader(),
+      $preview = $("#img_field");
+
+  reader.onload = (function(file) {
+    return function(e) {
+      $preview.empty();
+      $preview.append($('<img>').attr({
+        src: e.target.result,
+        width: "150px",
+        class: "preview",
+        title: file.name
+      }));
+    };
+  })(file);
+  reader.readAsDataURL(file);
+});
