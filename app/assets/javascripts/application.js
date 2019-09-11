93b15e6ae7e320192e0c521fed2ed81fdf2e6aea
morookaT <morooka.olopatadine@gmail.com>
2019-09-11 11:54:07 +0900

    [update]jqueryCDN記述を追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,21 +16,23 @@
 //= require turbolinks
 //= require_tree .
 
-$("#file").on("change",'input[type="file"]', function(e) {
-  var file = e.target.files[0],
-      reader = new FileReader(),
-      $preview = $("#img_field");
+$( document ).on('turbolinks:load', function(){
+  $("#file").on("change",'input[type="file"]', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $("#img_field");
 
-  reader.onload = (function(file) {
-    return function(e) {
-      $preview.empty();
-      $preview.append($('<img>').attr({
-        src: e.target.result,
-        width: "150px",
-        class: "preview",
-        title: file.name
-      }));
-    };
-  })(file);
-  reader.readAsDataURL(file);
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+          src: e.target.result,
+          width: "150px",
+          class: "preview",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
 });
