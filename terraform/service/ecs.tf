008d1647e448d75284ddac76d7803c25859211fd
morookaT <morooka.olopatadine@gmail.com>
2019-10-18 16:18:52 +0900

    [add]ロードバランサの記述を追加、sgの追加

--- terraform/service/ecs.tf
+++ terraform/service/ecs.tf
@@ -8,6 +8,12 @@ resource "aws_ecs_service" "rails-service" {
   task_definition = "${aws_ecs_task_definition.sample-task.arn}"
   desired_count   = 1
   launch_type     = "EC2"
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.http.arn}"
+    container_name   = "sample-rails"
+    container_port   = "3000"
+  }
 }
 
 resource "aws_ecs_task_definition" "sample-task" {
--- terraform/service/security_group.tf
+++ terraform/service/security_group.tf
@@ -7,6 +7,15 @@ resource "aws_security_group" "instance" {
   }
 
   ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    security_groups = [
+      "${aws_security_group.alb.id}",
+    ]
+  }
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -20,3 +29,26 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_security_group" "alb" {
+  name        = "sample-rails-alb"
+  description = "http and https"
+  vpc_id      = "${data.terraform_remote_state.vpc.outputs.vpc_id}"
+
+  tags = {
+    Name = "rails_alb_sg"
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
