2fdc1666aa0d839f4798e60bf6336076e9d6c606
morookaT <morooka.olopatadine@gmail.com>
2019-10-21 09:08:08 +0900

    [add]SSL証明の作成、[fix]shttpsを受け取るよう変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { protocol: "http", host: 'morooka3.net' }
+  config.action_mailer.default_url_options = { protocol: "https", host: 'morooka3.net' }
   # config.action_mailer.default_url_options = { protocol: 'https' , host: 'ec2-34-201-192-19.compute-1.amazonaws.com' }
   config.action_mailer.delivery_method = :ses
 end
